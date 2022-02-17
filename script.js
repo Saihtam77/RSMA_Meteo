3c4015fa8713729e83725e0b7a424b5774b47daf
Saihtam77 <pikathu18@gmail.com>
2022-02-17 16:12:35 -0400

    Integration des fonction météo

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
+
+//appelle des fonction
 function all() { //execute tout les fonction
     updateTime();
     updateData();
 }
-
-
-function refresh_data() {//timer pour l'actualisation des donnés
-    let t_data = 60000;
-    setTimeout('weather()', t_data)
-}
-
-function refresh_date() {//timer pour l'actualisatoin de l'horloge et de la date
-    let t_time = 1000;
-    setTimeout('updateTime()', t_time)
-}
-
-
 
 function updateTime() {//fonction pour l'horloge et la date
     showDate();
@@ -32,6 +21,17 @@ function updateData() { //fonction pour les gestion des donner météo
 
 
 //Creation des fonction//
+
+
+function refresh_data() {// fonction timer pour l'actualisation des donnés
+    let t_data = 60000;
+    setTimeout('weather()', t_data)
+}
+
+function refresh_date() {//fonction timer pour l'actualisatoin de l'horloge et de la date
+    let t_time = 1000;
+    setTimeout('updateTime()', t_time)
+}
 
 
 //Création de l'horloge et de la date
@@ -64,11 +64,19 @@ function showDate() {
 }
 
 
-//fonction météo
+// création des fonction météo
+
 function weather() { //Change les element donner en fonction de donnée
 
-    const météo = document.querySelector(".météo");
+    //capture de tous les balise html néccesaire
+    const meteo = document.querySelector(".météo");
+    const celcus = document.querySelector(".celcus :first-child");
+    const humidite = document.querySelector(".precipitation");
+    const vitesse_vent = document.querySelector(".humidite");
+    const precipitation = document.querySelector(".vitesse_vent");
 
+
+    //initialisation du fetch
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
@@ -79,6 +87,7 @@ function weather() { //Change les element donner en fonction de donnée
         cache: 'default'
     };
 
+    //Capture de L'API
     fetch("meteo.json", fetchInit)
         .then(data => { //success or not
             if (data.ok) {
@@ -94,14 +103,79 @@ function weather() { //Change les element donner en fonction de donnée
         .then(data => data.json())//Conversion en json
 
         .then(jsonData => {
+
             console.log(jsonData);
 
-            let today = {
+            const data_value = { //recup des donner de l'API
 
                 state: jsonData.TFFF.wx,
                 temperature: jsonData.TFFF.temp,
+                dewpoint: jsonData.TFFF.dewpoint,
+                vitesse: jsonData.TFFF.wind_vel,
+            }
+
+            const convert_value = { //Convertion des unité de certain valeur
+                humidité: humidity_calcl(data_value.dewpoint, data_value.temperature),
+                vitesseDuVent: vitesseVent_calcl(data_value.vitesse),
+            }
+
+            //affichage dans le console (Pour verifier)
+            console.log("Température:", data_value.temperature, "°")
+            console.log("Humidité de:", convert_value.humidité, "%")
+            console.log("Vent: ", convert_value.vitesseDuVent, "Km/h")
+            console.log(data_value.state)
+
+            //Ajout dans le DOM
+            celcus.textContent = (data_value.temperature + "°")
+            humidite.textContent = ("Humidité: " + convert_value.humidité + "%")
+            vitesse_vent.textContent = ("Vent: " + convert_value.vitesseDuVent + " Km/h")
+
+            switch (data_value.state) {
+                case "RA":
+                    meteo.src = "icone météo/rain.png";
+                    break;
+                case "TS":
+                    meteo.src = "icone météo/fort orage.png";
+                    break;
+                case "+RA":
+                    meteo.src = "icone météo/pluvieux.png";
+                    break;
+                case "SKC":
+                    meteo.src = "icone météo/ensolellier.png";
+                    break;
+                case "NSC":
+                    meteo.src = "icone météo/ensolellier.png";
+                    break;
+                case "FEW":
+                    meteo.src = "icone météo/nuageux.png";
+                    break;
+                case "SCT":
+                    meteo.src = "icone météo/nuageux.png"
+                    break;
+                case "BKN":
+                    meteo.src = "icone météo/nuageux.png"
+                    break;
+                case "OVC":
+                    meteo.src = "icone météo/nuageux.png"
+                    break;
+
 
             }
-            console.log(today.temperature)
+
         })
+
+    //Creation des fonction de convertion des unité
+    function humidity_calcl(dewpoint_value, temp_value) { //fonction de caclcule de l'humidité
+
+        humidity = -1 * (5 * (temp_value - dewpoint_value) - 100);
+        return humidity;
+    }
+
+    function vitesseVent_calcl(vitesseEnNoeud) {
+
+        kmParheure = vitesseEnNoeud * 1.852;
+        return kmParheure;
+
+    }
+
 }
